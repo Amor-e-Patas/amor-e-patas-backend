efbeaa74e2bed693cfb67cbe92bde62a947440a6
guilhermeg2k <guilhermen2k@gmail.com>
2021-08-13 14:34:50 -0300

    added all functions to the controller example

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,9 @@
 import express from 'express';
+import { Request, Response } from 'express';
 
 // Imports dos controllers
 import UserControllers from './controllers/user';
-import { Request, Response } from 'express';
+
 // Estanciamento dos controllers
 const userControllers = new UserControllers();
 
@@ -11,7 +12,12 @@ const routes = express.Router();
 routes.get('/', (req: Request, res: Response) => res.status(200).json("Hello world!"));
 
 // Criação das rotas para executar cada controller
+
 //Users
 routes.get('/users', userControllers.index);
+routes.get('/user/:id', userControllers.show)
+routes.post('/user', userControllers.create);
+routes.put('/user', userControllers.update);
+routes.delete('/user/:id', userControllers.delete);
 
 export default routes;
