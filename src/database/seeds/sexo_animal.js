77406bbdb4ff22ca55e750caa5f5d873e70bbf35
Uelyda Luiza <uelyda-luiza@hotmail.com>
2021-10-15 21:59:42 -0300

    🐛 Trocando os nomes do sexo do animal na seed

--- src/database/seeds/sexo_animal.js
+++ src/database/seeds/sexo_animal.js
@@ -5,8 +5,8 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('db_sexo_animal').insert([
-        {id_sexo: 1, tipo_sexo: 'Feminino'},
-        {id_sexo: 2, tipo_sexo: 'Masculino'}
+        {id_sexo: 1, tipo_sexo: 'Fêmea'},
+        {id_sexo: 2, tipo_sexo: 'Macho'}
       ]);
     });
 };
